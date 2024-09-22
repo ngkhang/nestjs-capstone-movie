6fc1fcbd0d4ed3bfaa075e6f8d33b72ad2ee97b6
Khang Nguyen <ngkhang0220@gmail.com>
2024-09-22 17:22:42 +0700

    setup: add config Prisma module, CORS and helmet
    Setup and configured Prisma ORM (Prisma module in Common module). Configured CORS and Helmet (basic)
    
    Ref: #1

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as express from 'express';
 
 import { setupSwagger } from './common/swagger/swagger.util';
 import { ConfigService } from './common/config/config.service';
+import helmet from 'helmet';
 
 async function bootstrap() {
   // Define port, config server
@@ -13,6 +14,17 @@ async function bootstrap() {
   // Setup Config module
   const configService = app.get(ConfigService);
   const { host, port } = configService.app;
+
+  // Setup helmet
+  app.use(helmet());
+
+  // Setup CORS
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-type, Accept',
+    credentials: true,
+  });
 
   // Define static assets folder
   app.use(express.static('.'));
